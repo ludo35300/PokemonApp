262f89c405f44b73976b9f2f96b76543c7ca4675
ludo35300 <ludo35300@gmail.com>
2024-02-23 09:00:46 +0100

    test2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener} from '@angular/core';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { POKEMONS } from './mock.pokemon';
+import { Pokemon } from './pokemon';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    NgIf, 
+    NgFor,
+    PokemonTypeColorPipe, 
+    BorderCardDirective
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'PokemonApp';
+
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected?: Pokemon;
+
+  ngOnInit(){
+      console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string){
+    /* + -> transforme string to number */
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId); 
+    if(pokemon){
+      console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}.`);
+      this.pokemonSelected = pokemon;
+    }else{
+      console.log(`Vous avez demandé un pokémon qui n'existe pas.`);
+      this.pokemonSelected = pokemon;
+    }
+  }
 }
+ 
---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,37 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[pkmBorderCard]',
+  standalone: true
+})
+export class BorderCardDirective {
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 180;
+
+  constructor(private el: ElementRef) {
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor)
+   }
+
+   @Input('pkmBorderCard') borderColor!: string ; // alias
+
+   // Change la couleur lors du survol
+   @HostListener('mouseenter') onMouseEnter(){
+    this.setBorder(this.borderColor || this.defaultColor);
+   }
+   // Change la couleur lorsqu'on quitte
+   @HostListener('mouseleave') onMouseLeave(){
+    this.setBorder(this.initialColor);
+   }
+
+
+  setHeight(height: number){
+    this.el.nativeElement.style.height = `${height}px`;
+  }
+
+  setBorder(color: string){
+    this.el.nativeElement.style.border = `solid 4px ${color}`;
+  }
+
+}
